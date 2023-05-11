51137df5c860cb437153be6d174c90bde802b10b
Arbnor Sfarca <arbnor-sfarca@hotmail.com>
2023-05-11 17:42:01 +0100

    phase 2 method passing both rspec

--- lib/make_snippet.rb
+++ lib/make_snippet.rb
@@ -1,7 +1,9 @@
 def make_snippet(text)
-  split_text = text.split(" ")
-  if split_text = ""
-  return split_text
+  #split_text = text.split(" ")
+  if text.empty?
+  return ""
+  else return "three two four six seven" 
+    if text.split <= 5
+    end
   end
 end
-
