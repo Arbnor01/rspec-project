f8b6e83441c2a31dd288786ccb89952d7d4e7dfb
Arbnor Sfarca <arbnor-sfarca@hotmail.com>
2023-05-11 18:31:47 +0100

    phase 2 TDD single methods challenge pass

--- lib/count_words.rb
+++ lib/count_words.rb
@@ -1,2 +1,12 @@
 def count_words(text)
-end
+ text.split.size
+ end
+ 
+
+# $ irb
+# irb(main):001:0> def count_words(text)
+# irb(main):002:1>   text.split.size
+# irb(main):003:1> end
+# => :count_words
+# irb(main):004:0> count_words("hello, my name is Arbnor")
+# => 5
