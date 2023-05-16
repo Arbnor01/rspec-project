c38bd4619d82cb84223127c53b6fc188f14bc30c
Arbnor Sfarca <arbnor-sfarca@hotmail.com>
2023-05-16 11:35:28 +0100

    challenge 1 - spec file with 2 test cases for error and returning true

---
+++ spec/task_tracker_spec.rb
@@ -0,0 +1,16 @@
+require 'task_tracker'
+
+RSpec.describe "task_tracker method" do
+  context "given an empty string" do
+    it "should raise and error" do
+      expect{task_tracker("")}.to raise_error "no text found"
+    end 
+  end
+
+  context "given a string with #TODO" do
+    it "returns true" do
+      expect(task_tracker("this string should have #TODO")).to eq(true)
+    end
+  end
+end
+
